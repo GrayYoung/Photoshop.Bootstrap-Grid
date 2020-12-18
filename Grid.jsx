cabb0298998e1e70579b91bebf229539fe36c21b
Gray <gray.yang@57blocks.io>
2020-12-18 15:45:58 +0800

    Fix the document's resolution cause incorrect dimension issue

--- Grid.jsx
+++ Grid.jsx
@@ -1,4 +1,4 @@
-/**
+﻿/**
  * Photoshop.Bootstrap-Grid
  * Version: 0.0.3
  * Author: Gray Young
@@ -10,11 +10,22 @@
 #include '../Photoshop.Utilities/Shape.jsx'
 
 (function() {
+	const savedRulerUnits = app.preferences.rulerUnits;
+	const resolution = app.activeDocument.resolution;
+	const scale = 72 / resolution;
 	var GROUP_NAME = 'Grid Columus', Message = {
 		EXISTED_GRID_LAYERSET: 'Grid Columus LayerSet is existed.'
 	};
 	var configFile = new File(new File($.fileName).path + '/bootstrap.config.json');
-	var config = null, i = 0, offsetA = 0; offsetB = 0; colWidth = 0, docHeight = 0;
+	var config = null,
+		containerWidth = 0,
+		gap = 0,
+		i = 0,
+		offsetA = 0;
+		offsetB = 0;
+		colWidth = 0,
+		docWidth = 0,
+		docHeight = 0;
 	// Photoshop Layer or Layer Group
 	var gridGroup = null, grid = null, layerSets = app.activeDocument.layerSets;
 
@@ -33,19 +44,25 @@
 	}
 
 	app.preferences.rulerUnits = Units.PIXELS;
-	docHeight = parseFloat(app.activeDocument.height);
-	colWidth = config.container / config.grid.columns - config.grid.gutter_width;
-	offsetA = (parseFloat(app.activeDocument.width) - config.container + config.grid.gutter_width) / 2;
-	offsetB = offsetA + colWidth;
+	docHeight = parseFloat(app.activeDocument.height) * scale;
+	docWidth = parseFloat(app.activeDocument.width) * scale;
+	containerWidth = config.container * scale;
+	gap = config.grid.gutter_width * scale;
+
+	colWidth = containerWidth / config.grid.columns - gap;
+	offsetA = (docWidth - containerWidth + gap) / 2;
+	offsetB = (offsetA + colWidth);
 	i = config.grid.columns;
 	gridGroup.name = GROUP_NAME;
 	gridGroup.opacity = config.grid.opacity;
+
 	do {
 		grid = gridGroup.artLayers.add();
 		grid.name = 'Grid ' + i;
 		Shape.vector.createRectangle([[offsetA, 0], [offsetB, 0], [offsetB, docHeight], [offsetA, docHeight]], Shape.COLOR[config.grid.color]);
-		offsetA = offsetB + config.grid.gutter_width;
+		offsetA = offsetB + gap;
 		offsetB = offsetA + colWidth;
 		i--;
 	} while(i > 0);
+	app.preferences.rulerUnits = savedRulerUnits;
 })();
